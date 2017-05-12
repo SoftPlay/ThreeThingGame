050b678938ddbf3efca83e0831e39edf03efa7c1
Eduards Brown <eduard.brown123@hotmail.com>
2017-05-12 17:54:23 +0100

    added game scene

---
+++ Fury/Assets/Scripts/Ground.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour 
+{
+	public int NumberOfPixels = 1;
+
+	private int PixelWidth;
+	private int ScreenWidth;
+
+	// Use this for initialization
+	void Start () 
+	{
+		ScreenWidth = Screen.width;
+
+		if(NumberOfPixels == 0)
+			NumberOfPixels = 1;
+			
+		PixelWidth = ScreenWidth / NumberOfPixels;
+		Debug.Log(PixelWidth);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
