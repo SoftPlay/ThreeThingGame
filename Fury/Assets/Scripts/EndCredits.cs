d16a771ca5712a38d68d5cd18d899cef517d61d6
Eduards Brown <eduard.brown123@hotmail.com>
2017-05-13 13:58:38 +0100

    done a thing

---
+++ Fury/Assets/Scripts/EndCredits.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndCredits : MonoBehaviour {
+
+	// Use this for initializationv
+	void OnMouseDown()
+    {
+        SceneManager.LoadScene("End Credit");
+    }
+}
